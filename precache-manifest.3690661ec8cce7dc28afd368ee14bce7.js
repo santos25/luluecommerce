b4bbcad60ea2e1bedd6fec57555a3f0203d4fd76
Andres Santos <santfer25@hotmail.com>
2020-07-06 16:43:10 -0500

    Updates

---
+++ precache-manifest.3690661ec8cce7dc28afd368ee14bce7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87f2c3cb69e51e1ad275032d261ec00",
+    "url": "/santos25/luluecommerce.git/index.html"
+  },
+  {
+    "revision": "e4119591f409e443a6d7",
+    "url": "/santos25/luluecommerce.git/static/css/2.45e9af46.chunk.css"
+  },
+  {
+    "revision": "e2818955ba773bd97680",
+    "url": "/santos25/luluecommerce.git/static/css/main.e394d3de.chunk.css"
+  },
+  {
+    "revision": "e4119591f409e443a6d7",
+    "url": "/santos25/luluecommerce.git/static/js/2.d1917945.chunk.js"
+  },
+  {
+    "revision": "06cd29af5d62e45a6166c345f2a8c804",
+    "url": "/santos25/luluecommerce.git/static/js/2.d1917945.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2818955ba773bd97680",
+    "url": "/santos25/luluecommerce.git/static/js/main.bab1564d.chunk.js"
+  },
+  {
+    "revision": "6e10e6ade899cbad7c51",
+    "url": "/santos25/luluecommerce.git/static/js/runtime-main.7dae4ce2.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/santos25/luluecommerce.git/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/santos25/luluecommerce.git/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/santos25/luluecommerce.git/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/santos25/luluecommerce.git/static/media/slick.f97e3bbf.svg"
+  }
+]);
