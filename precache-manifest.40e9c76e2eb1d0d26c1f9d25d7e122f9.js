6191fcbc4fe03227950c651d3c340c6fd15b7999
Andres Santos <asantos@dcatech.com>
2020-12-14 16:42:12 -0500

    Updates

---
+++ precache-manifest.40e9c76e2eb1d0d26c1f9d25d7e122f9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9caa606dfa39c21c24099aadee1f2ca",
+    "url": "/luluecommerce/index.html"
+  },
+  {
+    "revision": "34dd455e248d916c4438",
+    "url": "/luluecommerce/static/css/2.111bb465.chunk.css"
+  },
+  {
+    "revision": "27368ffe783132f0ab22",
+    "url": "/luluecommerce/static/css/main.8118b2fe.chunk.css"
+  },
+  {
+    "revision": "34dd455e248d916c4438",
+    "url": "/luluecommerce/static/js/2.44d875a2.chunk.js"
+  },
+  {
+    "revision": "dc64e19f402aedf17315ed795d66cb73",
+    "url": "/luluecommerce/static/js/2.44d875a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27368ffe783132f0ab22",
+    "url": "/luluecommerce/static/js/main.4317251e.chunk.js"
+  },
+  {
+    "revision": "fc51da0d0638e029db25",
+    "url": "/luluecommerce/static/js/runtime-main.f1a7c16f.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/luluecommerce/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/luluecommerce/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/luluecommerce/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/luluecommerce/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/luluecommerce/precache-manifest.90a945fd39f317f138520fe59dc05992.js"
+  "/luluecommerce/precache-manifest.40e9c76e2eb1d0d26c1f9d25d7e122f9.js"
 );
 
 self.addEventListener('message', (event) => {
